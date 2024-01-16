7139f8487609c4e293aac27c66483ddfffe8f102
Ryan Luis <RyanCLuis@gmail.com>
2024-01-16 06:41:12 -0800

    adds finches and index view for finches and migration

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
+from .models import Finch
+
 # Create your views here.
-finches = [
-    {'name': 'Birdy', 'breed': 'Star finch', 'description': 'furry little demon', 'age': 3},
-    {'name': 'Sachi', 'breed': 'House Finch', 'description': 'gentle and loving', 'age': 2},
-    {'name': 'Donut', 'breed': 'GoldFinch', 'description': 'cute but kindof scary', 'age': 0},
-]
+# finches = [
+#     {'name': 'Birdy', 'breed': 'Star finch', 'description': 'furry little demon', 'age': 3},
+#     {'name': 'Sachi', 'breed': 'House Finch', 'description': 'gentle and loving', 'age': 2},
+#     {'name': 'Donut', 'breed': 'GoldFinch', 'description': 'cute but kindof scary', 'age': 0},
+# ]
 
 
 def home(request):
@@ -15,4 +17,5 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', { 'finches' : finches })
