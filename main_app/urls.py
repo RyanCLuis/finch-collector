a53e7f7979b23c9e1b8b77843faa1a9b3246ef93
Ryan Luis <RyanCLuis@gmail.com>
2024-01-18 06:33:21 -0800

    can assoc toys on detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('toys/<int:pk>/update', views.ToyUpdate.as_view(), name='toys_update'),
     path('toys/<int:pk>/delete', views.ToyDelete.as_view(), name='toys_delete'),
     path('toys/<int:pk>', views.ToyDetail.as_view(), name='toys_detail'),
+    path('finches/<int:finch_id>/assoc_toy/<int:toy_id>', views.assoc_toy, name='assoc_toy'),
+    path('finches/<int:finch_id>/unassoc_toy/<int:toy_id>', views.unassoc_toy, name='unassoc_toy'),
 ]
